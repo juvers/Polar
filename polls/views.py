0a436eb3e4035e69a3fe7227ca992d7929b516cb
Jude kuti <judekuti@gmail.com>
2021-01-28 00:34:58 -0600

    Play with unseparated concerns

--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("Hello from index page")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You are looking at question %s." % question_id)
